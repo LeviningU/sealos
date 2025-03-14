7a66e5d6cc8576207e2d9954c4fd8e51a771ad8f
林峰 <lf02059301@alibaba-inc.com>
2025-03-14 18:46:09 +0800

    feat: add custom modal alert button style

--- frontend/providers/accountcenter/src/constants/theme.ts
+++ frontend/providers/accountcenter/src/constants/theme.ts
@@ -1,13 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
 import { theme as SealosTheme } from '@sealos/ui';
-import { cardAnatomy, checkboxAnatomy } from '@chakra-ui/anatomy';
+import { checkboxAnatomy } from '@chakra-ui/anatomy';
 import { createMultiStyleConfigHelpers } from '@chakra-ui/react';
+import { colors } from '@sealos/ui/src/theme/colors';
+import { Button } from '@sealos/ui/src/theme/components/Button';
+import { merge } from 'lodash';
 
 const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(
   checkboxAnatomy.keys
-);
-const { defineMultiStyleConfig: defineCardMultiStyleConfig } = createMultiStyleConfigHelpers(
-  cardAnatomy.keys
 );
 const checkbox = defineMultiStyleConfig({
   baseStyle: {
@@ -48,9 +48,28 @@ export const theme = extendTheme(SealosTheme, {
   },
   components: {
     Checkbox: checkbox,
-    Card: defineCardMultiStyleConfig({
+    // extend并不会deep merge会丢失sealos定义的button样式，这里deep merge以下
+    Button: merge(Button, {
+      sizes: {
+        md: {
+          h: '40px'
+        }
+      },
+      variants: {
+        danger: {
+          bg: '#DC2626',
+          color: '#fff',
+          _hover: {
+            bg: colors.red['600']
+          },
+          _active: {
+            bg: colors.red['700']
+          }
+        }
+      }
+    }),
+    Card: {
       baseStyle: {
-        red: {},
         container: {
           borderRadius: '16px',
           boxShadow: '0px 1px 2px 0px #0000000D'
@@ -65,6 +84,74 @@ export const theme = extendTheme(SealosTheme, {
           padding: '14px 24px 24px'
         }
       }
-    })
+    },
+    Modal: {
+      baseStyle: {
+        dialog: {
+          border: '1px solid #E4E4E7',
+          boxShadow: '0px 2px 4px -1px #0000000F,0px 4px 6px -1px #0000001A',
+          _before: {
+            content: '""',
+            position: 'absolute',
+            top: '-5px',
+            left: '-5px',
+            bottom: '-5px',
+            right: '-5px',
+            pointerEvents: 'none',
+            bg: '#F1F1F1',
+            borderRadius: '20px',
+            zIndex: 1
+          }
+        },
+        header: {
+          px: '24px',
+          pt: '24px',
+          pb: '16px',
+          bg: '#fff',
+          borderBottom: 'none',
+          fontSize: '20px',
+          fontWeight: 600,
+          zIndex: 2,
+          borderTopLeftRadius: '16px',
+          borderTopRightRadius: '16px'
+        },
+        body: {
+          p: '16px 24px',
+          bg: '#fff',
+          zIndex: 2
+        },
+        footer: {
+          px: '24px',
+          justifyContent: 'flex-start',
+          bg: '#fff',
+          zIndex: 2,
+          borderBottomLeftRadius: '16px',
+          borderBottomRightRadius: '16px'
+        },
+        overlay: {
+          backdropFilter: 'blur(10px)'
+        }
+      }
+    },
+    Alert: {
+      baseStyle: {
+        container: {
+          borderRadius: '8px',
+          p: '16px'
+        }
+      },
+      variants: {
+        danger: {
+          container: {
+            bg: '#FEF2F2',
+            color: '#DC2626'
+          },
+          description: {
+            fontSize: '14px',
+            lineHeight: '20px'
+          }
+        }
+      }
+    }
   }
 });
